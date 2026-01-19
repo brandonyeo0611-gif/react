7ece7b0275c21fda41842b228c32a4b0bcf059c0
Brandon <brandonyeo0611@gmail.com>
2026-01-19 21:43:38 +0800

    fix post not appearing

--- src/components/Forums.tsx
+++ src/components/Forums.tsx
@@ -54,8 +54,10 @@ export default function BasicStack({ category }: ForumsProps) {
         ); // get no need method cause fetch inherently already is get
         // fetch need
         const result = await response.json();
-        setPosts(result.payload.data || []);
-        console.log(result.payload.data);
+        if (result.errorCode === 0) {
+            setPosts(result.payload.data || []);
+            console.log(result.payload.data);
+        }
     };
     const handleClick = (postID: string) => {
         navigate(`/thread/${postID}`);
@@ -65,88 +67,90 @@ export default function BasicStack({ category }: ForumsProps) {
         setRetry(0);
         getforums();
     }, [category, relevance]);
-    return (
-        <Box sx={{ width: "flex", maxHeight: "" }}>
-            <Box alignItems="left" sx={{ display: "flex", justifyContent: "flex-start", alignItems: "flex-start" }}>
-                <FormControl
-                    sx={{
-                        "& .MuiOutlinedInput-root": {
-                            borderRadius: "10px",
-                        }, // make it a little rounder
-                        width: "20vw",
-                        maxWidth: "150px",
-                        mb: 2,
-                    }}
-                >
-                    <InputLabel id="demo-simple-select-label">Relevance</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        value={relevance}
-                        label="Relevance"
-                        onChange={handleChange}
+    if (posts) {
+        return (
+            <Box sx={{ width: "flex", maxHeight: "" }}>
+                <Box alignItems="left" sx={{ display: "flex", justifyContent: "flex-start", alignItems: "flex-start" }}>
+                    <FormControl
+                        sx={{
+                            "& .MuiOutlinedInput-root": {
+                                borderRadius: "10px",
+                            }, // make it a little rounder
+                            width: "20vw",
+                            maxWidth: "150px",
+                            mb: 2,
+                        }}
                     >
-                        <MenuItem value={"mostRecent"}>Most Recent</MenuItem>
-                        <MenuItem value={"leastRecent"}>Least Recent</MenuItem>
-                        <MenuItem value={"mostLikes"}>Most likes</MenuItem>
-                        <MenuItem value={"leastLikes"}>Least likes</MenuItem>
-                    </Select>
-                </FormControl>
+                        <InputLabel id="demo-simple-select-label">Relevance</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={relevance}
+                            label="Relevance"
+                            onChange={handleChange}
+                        >
+                            <MenuItem value={"mostRecent"}>Most Recent</MenuItem>
+                            <MenuItem value={"leastRecent"}>Least Recent</MenuItem>
+                            <MenuItem value={"mostLikes"}>Most likes</MenuItem>
+                            <MenuItem value={"leastLikes"}>Least likes</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Box>
+                <Box sx={{ width: "flex" }}>
+                    <Stack useFlexGap spacing={3}>
+                        {posts.map((post) => (
+                            <Item key={post.post_id} onClick={() => handleClick(post.post_id)}>
+                                <Typography variant="h6">{post.title}</Typography>
+                                <Divider sx={{ my: 0.2 }} />
+                                <Typography
+                                    variant="body2"
+                                    sx={{
+                                        display: "-webkit-box",
+                                        WebkitBoxOrient: "vertical",
+                                        overflow: "hidden",
+                                        textOverflow: "ellipsis",
+                                        WebkitLineClamp: 3,
+                                        // if want to overflow and ... there is more then 1 line.. cause noWrap only gives one line
+                                    }}
+                                >
+                                    {post.content}
+                                </Typography>
+                                <Divider sx={{ my: 0.2 }} />
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                        alignItems: "center",
+                                        borderRadius: 2,
+                                        bgcolor: "background.paper",
+                                        color: "text.secondary",
+                                        "& svg": {
+                                            m: 1,
+                                        },
+                                    }}
+                                >
+                                    <Box sx={{ flex: 1 }}>
+                                        <Typography variant="subtitle2" sx={{ p: 1 }}>
+                                            Author : {post.username}
+                                        </Typography>
+                                    </Box>
+                                    <Divider orientation="vertical" variant="middle" flexItem />
+                                    <Box sx={{ flex: 1 }}>
+                                        <Typography variant="subtitle2" sx={{ p: 1 }}>
+                                            Tag : {post.content_type}
+                                        </Typography>
+                                    </Box>
+                                    <Divider orientation="vertical" variant="middle" flexItem />
+                                    <Box sx={{ flex: 1 }}>
+                                        <Typography variant="subtitle2" sx={{ p: 1 }}>
+                                            Likes: {post.likes}
+                                        </Typography>
+                                    </Box>
+                                </Box>
+                            </Item>
+                        ))}
+                    </Stack>
+                </Box>
             </Box>
-            <Box sx={{ width: "flex" }}>
-                <Stack useFlexGap spacing={3}>
-                    {posts.map((post) => (
-                        <Item key={post.post_id} onClick={() => handleClick(post.post_id)}>
-                            <Typography variant="h6">{post.title}</Typography>
-                            <Divider sx={{ my: 0.2 }} />
-                            <Typography
-                                variant="body2"
-                                sx={{
-                                    display: "-webkit-box",
-                                    WebkitBoxOrient: "vertical",
-                                    overflow: "hidden",
-                                    textOverflow: "ellipsis",
-                                    WebkitLineClamp: 3,
-                                    // if want to overflow and ... there is more then 1 line.. cause noWrap only gives one line
-                                }}
-                            >
-                                {post.content}
-                            </Typography>
-                            <Divider sx={{ my: 0.2 }} />
-                            <Box
-                                sx={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                    borderRadius: 2,
-                                    bgcolor: "background.paper",
-                                    color: "text.secondary",
-                                    "& svg": {
-                                        m: 1,
-                                    },
-                                }}
-                            >
-                                <Box sx={{ flex: 1 }}>
-                                    <Typography variant="subtitle2" sx={{ p: 1 }}>
-                                        Author : {post.username}
-                                    </Typography>
-                                </Box>
-                                <Divider orientation="vertical" variant="middle" flexItem />
-                                <Box sx={{ flex: 1 }}>
-                                    <Typography variant="subtitle2" sx={{ p: 1 }}>
-                                        Tag : {post.content_type}
-                                    </Typography>
-                                </Box>
-                                <Divider orientation="vertical" variant="middle" flexItem />
-                                <Box sx={{ flex: 1 }}>
-                                    <Typography variant="subtitle2" sx={{ p: 1 }}>
-                                        Likes: {post.likes}
-                                    </Typography>
-                                </Box>
-                            </Box>
-                        </Item>
-                    ))}
-                </Stack>
-            </Box>
-        </Box>
-    );
+        );
+    }
 }
